682bef212fc822c79e1a07d35af11331ff80ff4e
Chris <chrisandrew119@gmail.com>
2018-12-28 17:57:20 +0530

    Change params.

--- hparams.py
+++ hparams.py
@@ -79,7 +79,7 @@ def create_hparams(hparams_string=None, verbose=False):
         learning_rate=1e-3,
         weight_decay=1e-6,
         grad_clip_thresh=1,
-        batch_size=12,
+        batch_size=4,
         mask_padding=False  # set model's padded outputs to padded values
     )
 
